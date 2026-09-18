e9759cff1c3724535c87e28bf5c6f0d6377b5bb6
KSRawat836 <karanrawat080306@gmail.com>
2026-09-18 20:42:51 +0530

    Add retrieve.py: query embedding and Chroma similarity search

---
+++ core/retrieve.py
@@ -0,0 +1,19 @@
+import chromadb
+from ingest import model
+from config import VECTORDB_PATH, COLLECTION_NAME, TOP_K
+
+def retrieve(query):
+    query_embeddings  = model.encode([query])
+    chro = chromadb.PersistentClient(path = VECTORDB_PATH)
+    collection = chro.get_or_create_collection(name= COLLECTION_NAME)
+
+    results = collection.query(
+        query_embeddings = query_embeddings.tolist(),
+        n_results = TOP_K
+        )
+    return results
+
+
+if __name__ == "__main__":
+    results = retrieve("who is Zorblatt?")
+    print(results)
